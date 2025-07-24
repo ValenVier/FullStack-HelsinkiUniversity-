ed882c7ac278c3d1bf058c6ae06905264ecd4ced
Javier Rodríguez Valentín <javrova@gmail.com>
2025-07-24 03:14:50 +0200

    Add changes in part1

--- Training/part1/src/App.jsx
+++ Training/part1/src/App.jsx
@@ -1,34 +1,55 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+const Hello = () => { return (<div><p>Hello world</p></div>) }/* New component, could be called in the app component */
+const Hello2 = (props) => {
+  return (
+    <div>
+      <p>Hello {props.name}</p>
+    </div>
+  )
+}/* New component with props */
 
-function App() {
-  const [count, setCount] = useState(0)
+const Hello3 = (props) => {
+  console.log(props)
+  return (
+    <div>
+      <p>Hello {props.name}, you are {props.age}</p>
+    </div>
+  )
+}/* New component with props */
+
+const App = () => {
+  const name = 'Peter'
+  const age = 10
+
+  const friends = [
+    {name:'Anna', age: 4},
+    {name:'Janet', age: 25},
+  ]
+
+  const friends2 = [
+    'Colen', 'Nanes'
+  ]
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <h1>Greetings</h1>
+      <Hello />
+
+      <Hello2 name='George' /* tag with props */ />
+      <Hello2 name='Daisy' /* tag with props */ />
+
+      <Hello3 name='Maya' age={26 + 10}/>
+      <Hello3 name={name} age={age}></Hello3>
+
+      {/* Following code will show an error */}
+      {/* <p>{friends[0]}</p>
+      <p>{friends[1]}</p> */}
+
+      <p>{friends[0].name} {friends[0].age}</p>
+      <p>{friends[1].name} {friends[1].age}</p>
+
+      <p>{friends2}</p>
+
+    </div>
   )
 }
 
