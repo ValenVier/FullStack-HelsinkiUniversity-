71bf73485a4dfa174991c07e5f1b725642c0bbe4
Javier Rodríguez Valentín <javrova@gmail.com>
2025-07-27 13:19:00 +0200

    UniCafe1.7

--- Exercises/Part1/unicafe/src/App.jsx
+++ Exercises/Part1/unicafe/src/App.jsx
@@ -8,11 +8,14 @@ const Button = ({onClick, text}) => (
   </button>
 )
 
-const Count = ({ good, neutral, bad }) => <div>
+const Count = ({ good, neutral, bad, total }) => <div>
       <h2>Statistics</h2>
       <p>Good {good}</p>
       <p>Neutral {neutral}</p>
       <p>Bad {bad}</p>
+      <p>All {total}</p>
+      <p>Average {total/3}</p>
+      <p>Positive {good/total * 100} %</p>
     </div>
 
 const App = () => {
@@ -21,10 +24,13 @@ const App = () => {
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
+  const [total, setTotal] = useState(0)
+
   const setToValue = (newValue, setFunction) => {
     //console.log('value now', newValue)
     //console.log('setFunction', setFunction)
     setFunction(newValue)
+    setTotal(total + 1);
   }
 
   return (
@@ -35,7 +41,7 @@ const App = () => {
       <Button onClick={() => setToValue(neutral + 1, setNeutral)} text="Neutral" />
       <Button onClick={() => setToValue(bad + 1, setBad)} text="Bad" />
 
-      <Count good={good} neutral={neutral} bad={bad}/>
+      <Count good={good} neutral={neutral} bad={bad} total={total}/>
     </div>
   )
 }
