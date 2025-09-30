cd4da8c77fdff4e38d0ee3c108a9c4f6cd96b831
ValenVier <javrova@gmail.com>
2025-09-30 15:15:04 +0200

    Phonebook 2.11 - Commit1

--- Exercises/Part2/phonebook/src/App.jsx
+++ Exercises/Part2/phonebook/src/App.jsx
@@ -1,18 +1,23 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import Filter from './components/Filter'
-import PersonForm from './components/NewPersonForm'
-import Persons from './components/PeopleList'
+import NewPersonForm from './components/NewPersonForm'
+import PeopleList from './components/PeopleList'
+import axios from 'axios'
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ]) 
+  const [persons, setPersons] = useState([]) //empty at the beginning
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [filter, setFilter] = useState('')
+
+  //useEffect to load data from the server
+  useEffect(() => {
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        setPersons(response.data)
+      })
+  }, []) //Empty array as dependency = runs only when mounting the component
 
   const addPerson = (event) => {
     event.preventDefault()
@@ -55,7 +60,7 @@ const App = () => {
       <h2>Phonebook</h2>
       <Filter value={filter} onChange={handleFilter} />
       <h3>Add a new</h3>
-      <PersonForm 
+      <NewPersonForm 
         onSubmit={addPerson}
         nameValue={newName}
         onNameChange={handleNameChange}
@@ -63,7 +68,7 @@ const App = () => {
         onNumberChange={handleNumberChange}
       />
       <h3>Numbers</h3>
-      <Persons persons={personsToShow} />
+      <PeopleList persons={personsToShow} />
     </div>
   )
 }
